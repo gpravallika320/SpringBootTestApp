ab16e07afbec614949ac8e2fd32a3bdd36f59870
visali <visali@visali-PC>
2021-05-18 09:22:06 +0530

    Test-101-third commit

--- src/main/java/com/example/demo/MessageRunner.java
+++ src/main/java/com/example/demo/MessageRunner.java
@@ -1,13 +1,22 @@
 package com.example.demo;
 
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 @Component
 public class MessageRunner implements CommandLineRunner {
-
-	@Override
+private static Logger log=LoggerFactory.getLogger(MessageRunner.class);
+@Override
 	public void run(String... args) throws Exception {
 		int a=10;
+		try {
+			log.info("started");
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
         System.out.println("Hello Git code");
 	}
 
